b7801c0ca514a9d580e47c575ec21cc3ae39507a
Jacques Pienaar <jpienaar@google.com>
2023-01-04 22:12:48 -0800

    Add C++ user (and fix errors reported)

---
+++ examples/MlirBuilder/Builder.cpp
@@ -0,0 +1,19 @@
+//===- Builder.cpp - Testing bytecode reader ------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir-c/IR.h"
+#include "mlir-c/Support.h"
+
+// Include bytecode parsing implementation.
+#include "mlirbcc/Parse.c.inc"
+// Dialects.
+#include "mlirbcc/BuiltinParse.c.inc"
+
+int main() {
+  return 0;
+}
